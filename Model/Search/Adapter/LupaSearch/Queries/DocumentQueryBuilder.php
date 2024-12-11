3e1241fd7b02c35c306e4664c000302b2646e71a
dovydas altaravicius <dovydas@adeoweb.biz>
2024-12-11 15:15:27 +0200

    Use PublicQuery for search

--- Model/Search/Adapter/LupaSearch/Queries/DocumentQueryBuilder.php
+++ Model/Search/Adapter/LupaSearch/Queries/DocumentQueryBuilder.php
@@ -6,6 +6,7 @@ namespace LupaSearch\LupaSearchPlugin\Model\Search\Adapter\LupaSearch\Queries;
 
 use LupaSearch\LupaSearchPluginCore\Api\Data\SearchQueries\DocumentQueryInterface;
 use LupaSearch\LupaSearchPluginCore\Api\Data\SearchQueries\DocumentQueryInterfaceFactory as DocumentQueryFactory;
+use LupaSearch\LupaSearchPlugin\Model\Search\Adapter\LupaSearch\Queries\SearchQuery\Configuration\SortBuilderInterface;
 use Magento\Framework\Search\RequestInterface;
 
 class DocumentQueryBuilder implements DocumentQueryBuilderInterface
@@ -14,14 +15,18 @@ class DocumentQueryBuilder implements DocumentQueryBuilderInterface
 
     private DocumentQueryFactory $documentQueryFactory;
 
-    public function __construct(DocumentQueryFactory $documentQueryFactory)
+    private SortBuilderInterface $sortBuilder;
+
+    public function __construct(DocumentQueryFactory $documentQueryFactory,  SortBuilderInterface $sortBuilder)
     {
         $this->documentQueryFactory = $documentQueryFactory;
+        $this->sortBuilder = $sortBuilder;
     }
 
     public function build(?RequestInterface $request = null): DocumentQueryInterface
     {
         $documentQuery = $this->documentQueryFactory->create();
+        $documentQuery->setSort($this->sortBuilder->build($request));
         $documentQuery->setLimit(
             $request && $request->getSize() <= self::LIMIT_MAX ? (int)$request->getSize() : self::LIMIT_MAX
         );
